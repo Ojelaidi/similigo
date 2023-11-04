52b3a748b17c26cf14665191ec7f04ba78e5f62b
ojelaidi <ojelaidi@figarocms.fr>
2023-11-04 10:37:20 +0100

    cleanUp

--- similarity/containmentSimilarity.go
+++ similarity/containmentSimilarity.go
@@ -5,21 +5,6 @@ import (
 	"strings"
 )
 
-/*func ContainmentSimilarity(text1, text2 string) float64 {
-	keywordTokens := strings.Fields(strings.ToLower(text1))
-	titleTokens := strings.Fields(strings.ToLower(text2))
-
-	matchCount := 0.0
-	for _, kToken := range keywordTokens {
-		for _, tToken := range titleTokens {
-			if strings.Contains(kToken, tToken) || strings.Contains(tToken, kToken) {
-				matchCount++
-			}
-		}
-	}
-	return matchCount
-}*/
-
 func ContainmentSimilarity(text1, text2 string) float64 {
 	set1 := make(map[string]bool)
 	set2 := make(map[string]bool)
